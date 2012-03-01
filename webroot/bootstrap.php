3cbf00e7513252b1a85844800f9ec31e26cb0c48
Webber Wu <webber@pixnet.tw>
2012-03-01 15:46:04 +0800

    add bootstrap.php

---
+++ webroot/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+$server_name = $_SERVER['SERVER_NAME'];
+$node = explode('.', $server_name);
+if ($node[count($node) - 1] == 'pixnet') {
+    define('DOMAIN_APPEND', '.' . implode('.', array_slice($node, -3)));
+} else {
+    define('DOMAIN_APPEND', '');
+}
+
+$baseDir = __DIR__;
+set_include_path(get_include_path() . PATH_SEPARATOR . "$baseDir/../libs");
+
+if (DOMAIN_APPEND && file_exists("$baseDir/../debug.php")) {
+    require_once("$baseDir/../debug.php");
+}
---
+++ webroot/index.php
@@ -0,0 +1,3 @@
+<?php
+require_once('bootstrap.php');
+require_once('PixAPI.php');
