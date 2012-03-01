89f2a58cd069d7c8b9fab239145bab23c48e504c
Webber Wu <webber@pixnet.tw>
2012-03-01 15:53:27 +0800

    寫個 emma api 來測試 heroku config 用

--- webroot/index.php
+++ webroot/index.php
@@ -1,3 +1,14 @@
 <?php
 require_once('bootstrap.php');
 require_once('PixAPI.php');
+
+$api = new PixAPI(CONSUMER_KEY, CONSUMER_SECRET);
+$api->setToken(ACCESS_TOKEN, ACCESS_SECRET);
+if (DOMAIN_APPEND) {
+    $api->setCurlOptions(array(
+        CURLOPT_PROXY => 'proxy.srv.pixnet:3128'
+    ));
+}
+
+//print_r(json_decode($api->http('http://emma.pixnet.cc/album/sets/16254219', array('get_params' => array('user' => 'movieca')))));
+print_r(json_decode($api->http('http://emma.pixnet.cc/album/elements', array('get_params' => array('set_id' => 16254219, 'type' => 'pic', 'with_detail' => 1)))));
