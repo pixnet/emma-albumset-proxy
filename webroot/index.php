825977a930df2b95323044f4a22d4bb28c9817ac
Webber Wu <webber@pixnet.tw>
2012-03-02 11:00:46 +0800

    style

--- webroot/index.php
+++ webroot/index.php
@@ -10,9 +10,7 @@ if ($_GET['sig'] != crc32(SHARED_SECRET . $_GET['set_id'])) {
 $api = new PixAPI(CONSUMER_KEY, CONSUMER_SECRET);
 $api->setToken(ACCESS_TOKEN, ACCESS_SECRET);
 if (DOMAIN_APPEND) {
-    $api->setCurlOptions(array(
-        CURLOPT_PROXY => 'proxy.srv.pixnet:3128'
-    ));
+    $api->setCurlOptions(array(CURLOPT_PROXY => 'proxy.srv.pixnet:3128'));
 }
 
 //print_r(json_decode($api->http('http://emma.pixnet.cc/album/sets/16254219', array('get_params' => array('user' => 'movieca')))));
